9738d4b411707028607aee91d00d9c8e46162dc1
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-08-16 23:36:51 +0800

    feat: Add basic react-navigation configuration with search screen

--- App.js
+++ App.js
@@ -1,23 +1,17 @@
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Constants from 'expo-constants'
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+import SearchScreen from './src/screens/SearchScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! </Text>
-      <StatusBar style="auto" />
-      <Text>{Constants.manifest.extra.YELP_API_KEY}</Text>
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
   },
-})
+  {
+    initialRouteName: 'Search',
+    defaultNavigationOptions: {
+      title: 'BusinessSearch',
+    },
+  },
+)
+
+export default createAppContainer(navigator)
