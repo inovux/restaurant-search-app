bf18cb76029e9a4bcb48db5e3cdbe2605e5cb70f
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-08-16 20:11:28 +0800

    feat: Add .env configuration

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Constants from 'expo-constants'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! </Text>
       <StatusBar style="auto" />
+      <Text>{Constants.manifest.extra.YELP_API_KEY}</Text>
     </View>
   )
 }
---
+++ app.config.js
@@ -0,0 +1,7 @@
+import 'dotenv/config'
+
+export default {
+  extra: {
+    YELP_API_KEY: process.env.YELP_API_KEY,
+  },
+}
