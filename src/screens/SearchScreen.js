3a786a596f9ed83500e886d5fb9af43f3db7ef86
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-08-18 22:35:10 +0800

    feat: Add search request to search screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,32 @@
 import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
+import yelp from '../api/yelp'
 
 const SearchScreen = () => {
   const [searchTerm, setSearchTerm] = useState('')
+  const [restaurants, setRestaurants] = useState('')
+
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term: searchTerm,
+        location: 'hong kong',
+      },
+    })
+
+    setRestaurants(response.data.businesses)
+  }
 
   return (
     <View>
       <SearchBar
         searchTerm={searchTerm}
         setSearchTerm={setSearchTerm}
-        onSubmitSearchTerm={() => console.log('Term was submitted')}
+        onSubmitSearchTerm={searchApi}
       />
-      <Text>{searchTerm}</Text>
+      <Text>We have found {restaurants.length} results</Text>
     </View>
   )
 }
