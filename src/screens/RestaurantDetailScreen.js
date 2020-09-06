026c489b7fa52f521f3fcf4734a68e14dbbd07b9
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-09-06 22:31:36 +0800

    feat: Add reviews feature

--- src/screens/RestaurantDetailScreen.js
+++ src/screens/RestaurantDetailScreen.js
@@ -4,6 +4,7 @@ import yelp from '../api/yelp'
 
 const RestaurantDetailScreen = ({ navigation }) => {
   const [restaurant, setRestaurant] = useState(null)
+  const [reviews, setReviews] = useState(null)
   const id = navigation.getParam('id')
 
   const getRestaurant = async (id) => {
@@ -12,11 +13,18 @@ const RestaurantDetailScreen = ({ navigation }) => {
     setRestaurant(response.data)
   }
 
+  const getReviews = async (id) => {
+    const response = await yelp.get(`/${id}/reviews`)
+
+    setReviews(response.data)
+  }
+
   useEffect(() => {
     getRestaurant(id)
+    getReviews(id)
   }, [])
 
-  if (!restaurant) {
+  if (!restaurant || !reviews) {
     return null
   }
 
@@ -41,16 +49,48 @@ const RestaurantDetailScreen = ({ navigation }) => {
           return <Image style={styles.image} source={{ uri: item }} />
         }}
       />
+      <Text style={styles.title}>Reviews</Text>
+      <FlatList
+        data={reviews.reviews}
+        style={styles.container}
+        keyExtractor={(review) => review.id}
+        renderItem={({ item }) => {
+          return (
+            <>
+              <Text styles={styles.description}>
+                {'\n'}
+                {item.text}
+              </Text>
+              <Text style={styles.reviewDate}>
+                {item.time_created} {'\n'}
+              </Text>
+              <View
+                style={{
+                  borderBottomColor: 'black',
+                  borderBottomWidth: 1,
+                }}
+              />
+            </>
+          )
+        }}
+      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  reviewDate: {
+    textAlign: 'right',
+  },
   title: {
     marginVertical: 5,
     marginHorizontal: 15,
     fontSize: 24,
     fontWeight: 'bold',
+  },
+  container: {
+    marginBottom: 10,
+    marginHorizontal: 15,
   },
   description: {
     marginBottom: 10,
